63495c20af82e228c67ed2ea051651bcc8ef6780
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2024-10-14 17:24:10 +0700

    add shadcn tabs

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -1,5 +1,22 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 function Portfolio() {
-  return <div>Portfolio</div>;
+  return (
+    <div className="flex flex-col items-center h-[calc(100vh-10rem)]">
+      <Tabs defaultValue="all">
+        <TabsList>
+          <TabsTrigger value="all">All</TabsTrigger>
+          <TabsTrigger value="react-js">React JS</TabsTrigger>
+          <TabsTrigger value="next-js">Next JS</TabsTrigger>
+          <TabsTrigger value="react-native">React Navive</TabsTrigger>
+        </TabsList>
+        <TabsContent value="all">All Section</TabsContent>
+        <TabsContent value="react-js">React JS Section</TabsContent>
+        <TabsContent value="next-js">Next JS Section</TabsContent>
+        <TabsContent value="react-native">React Native Section</TabsContent>
+      </Tabs>
+    </div>
+  );
 }
 
 export default Portfolio;
