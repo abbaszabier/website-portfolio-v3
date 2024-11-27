6eeb668ce3b79c35d9ccdf8151d2aecdc74e2499
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2024-11-27 19:49:51 +0700

    fix ui Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,9 @@
 import { Helmet } from "react-helmet-async";
-import { useNavigate } from "react-router-dom";
 import Profile from "@/assets/profile.webp";
+import SocialButton from "@/components/SosialButton.tsx";
+import { Download, Mail } from "lucide-react";
 
 function Home() {
-  const navigate = useNavigate();
   const phoneNumber = "6288211156895";
   const message =
     "Hi, my name is (your name) from (your country). I would like to hire you. Are you currently available?";
@@ -17,17 +17,17 @@ function Home() {
         <title>Portfolio Abbas - Home</title>
       </Helmet>
       <div className="flex flex-col md:flex-row sm:justify-between xs:justify-center items-center h-[calc(100vh-10rem)] sm:px-12 xs:px-6 xs:pb-6">
-        <div className="relative flex w-full md:w-1/3 justify-center items-center order-1 md:order-2 xs:mb-4">
+        <div className="relative flex md:w-1/3 justify-center rounded-full shadow-lg bg-white dark:bg-[#051c29] items-center order-1 md:order-2 xs:mb-4">
           <img
             src={Profile}
             alt="Avatar"
             loading="lazy"
             width="384"
             height="384"
-            className="w-60 xs:w-50 md:w-72 lg:w-80 xl:w-96 mx-auto rounded-full shadow-lg bg-white dark:bg-[#051c29]"
+            className="w-60 xs:w-48 md:w-72 lg:w-80 xl:w-96 mx-auto transition duration-300 ease-in-out hover:scale-110"
           />
         </div>
-        <div className="flex w-full md:w-1/2 flex-col justify-start gap-4 order-2 md:order-1">
+        <div className="flex w-full md:w-1/2 flex-col gap-4 order-2 md:order-1">
           <h1 className="text-[24px] xs:text-center sm:text-[26px] md:text-[28px] lg:text-[32px] xl:text-[36px] 2xl:text-[52px] font-light text-gray-900 dark:text-gray-200 leading-snug">
             Hi!{" "}
             <span className="font-bold text-[#051c29] dark:text-white">
@@ -47,22 +47,39 @@ function Home() {
             design, coffee, and music. I have a passion for creating beautiful
             and responsive websites.
           </p>
-          <div className="flex flex-row gap-4 xs:gap-2">
+          <div className="flex flex-row gap-4 xs:gap-2 xs:flex-col xs:justify-center sm:justify-left items-center">
             <button
               onClick={() => {
                 window.open(waLink, "_blank");
               }}
-              className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 shadow bg-[#051c29] text-[#ffffff] font-semibold dark:bg-white dark:text-[#051c29] rounded-full"
+              className="relative mr-1 rounded-full inline-block font-semibold group p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 shadow"
             >
-              Hire Me
+              {/* Lapisan efek hover */}
+              <span className="absolute inset-0 w-full h-full transition duration-400 ease-out transform translate-x-1 translate-y-1 bg-[#051c29] dark:bg-white group-hover:-translate-x-0 group-hover:-translate-y-0 rounded-full"></span>
+
+              {/* Lapisan utama dengan border */}
+              <span className="absolute inset-0 w-full h-full bg-white dark:bg-[#051c29] border border-[#051c29] dark:border-white group-hover:bg-gray-100 dark:group-hover:bg-gray-800 rounded-full"></span>
+
+              {/* Konten tombol */}
+              <span className="relative text-[#051c29] dark:text-white">
+                Hire Me
+              </span>
             </button>
 
-            <button
-              onClick={() => navigate("/contact")}
-              className="p-2 w-full sm:w-1/2 md:w-1/3 rounded-full lg:w-1/4 shadow border border-[#051c29] text-[#051c29] font-semibold dark:border-white dark:text-[#ffffff]"
-            >
-              Contact
-            </button>
+            <span className="text-[#051c29] xs:hidden dark:text-white">-</span>
+
+            <div className="flex w-full space-x-2 xs:mt-2 xs:justify-center">
+              <SocialButton
+                icon={<Download size={14} />}
+                label="Resume"
+                href="/resume.pdf"
+              />
+              <SocialButton
+                icon={<Mail size={14} />}
+                label="Email"
+                href="mailto:abbas.zabier06@gmail.com"
+              />
+            </div>
           </div>
         </div>
       </div>
