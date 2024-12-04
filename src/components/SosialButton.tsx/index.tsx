7e6bd2680e995967da4fd0308e6e82addfafdd71
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2024-12-04 09:08:17 +0700

    fix ui sosialbutton

--- src/components/SosialButton.tsx/index.tsx
+++ src/components/SosialButton.tsx/index.tsx
@@ -20,7 +20,7 @@ const SocialButton: FC<SocialButtonProps> = ({ icon, label, href }) => {
         target="_blank"
         rel="noopener noreferrer"
         className="relative group overflow-hidden rounded-full shadow dark:border-white text-[#051c29] dark:text-white 
-          px-2 py-2 text-lg font-semibold  bg-white transition duration-300"
+          px-2 py-2 text-lg font-semibold  bg-white dark:bg-[#051c29] transition duration-300"
       >
         {/* Background animasi */}
         <span className="absolute inset-0 bg-[#051c29] dark:bg-white transition-transform duration-300 transform translate-x-full translate-y-full group-hover:translate-x-0 group-hover:translate-y-0 rounded-full"></span>
