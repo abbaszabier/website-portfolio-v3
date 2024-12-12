6e8f6f36fdbb555b5ca98f107b1021675bd4b84a
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2024-12-12 11:15:54 +0700

    update main layout

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -114,15 +114,15 @@ function MainLayout() {
     <div className="relative flex flex-col lg:flex-row-reverse lg:flex-row lg:p-6 bg-gray-100 dark:bg-gray-900 min-h-screen">
       <div className="absolute inset-0 z-0 overflow-hidden">
         {/* Shape 1 */}
-        <div className="absolute w-96 h-96 xs:w-48 xs:h-48 bg-gradient-to-r from-blue-400 to-purple-600 rounded-full opacity-50 blur-3xl animate-moveShape1"></div>
+        <div className="absolute w-96 h-96 xs:w-48 xs:h-48 bg-gradient-to-r from-blue-400 to-purple-600 rounded-full opacity-30 blur-lg animate-moveShape1 will-change-transform"></div>
         {/* Shape 2 */}
-        <div className="absolute w-72 h-72 xs:w-40 xs:h-40 bg-gradient-to-r from-green-400 to-teal-600 rounded-lg opacity-50 blur-3xl animate-moveShape2"></div>
+        <div className="absolute w-72 h-72 xs:w-40 xs:h-40 bg-gradient-to-r from-green-400 to-teal-600 rounded-full opacity-30 blur-lg animate-moveShape2 will-change-transform"></div>
         {/* Shape 3 */}
-        <div className="absolute w-64 h-64 xs:w-36 xs:h-36 bg-gradient-to-r from-pink-400 to-red-600 rounded-full opacity-50 blur-3xl animate-moveShape3"></div>
+        <div className="absolute w-64 h-64 xs:w-36 xs:h-36 bg-gradient-to-r from-pink-400 to-red-600 rounded-full opacity-30 blur-lg animate-moveShape3 will-change-transform"></div>
       </div>
 
       {/* Main content area */}
-      <div className="relative z-10 flex-1 lg:ml-6 lg:rounded-xl overflow-y-auto border border-gray-200 shadow-sm backdrop-blur-sm bg-opacity-75 bg-white dark:bg-opacity-75 dark:bg-gray-800 dark:border-gray-700">
+      <div className="relative z-10 flex-1 lg:ml-6 lg:rounded-xl sm:overflow-y-auto h-[calc(100vh-3rem)] border border-gray-200 shadow-sm backdrop-blur-sm bg-opacity-80 bg-white dark:bg-opacity-75 dark:bg-gray-800 dark:border-gray-700">
         <div className="sticky top-0 right-0 z-50 justify-end flex px-6 pt-6 w-full">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
@@ -170,13 +170,13 @@ function MainLayout() {
             <DropdownMenuContent
               className={`mr-12 xs:mr-6 ${
                 darkMode
-                  ? "bg-[#051c29] bg-opacity-75 backdrop-blur-sm text-white"
-                  : "bg-white bg-opacity-75 backdrop-blur-sm text-[#051c29]"
+                  ? "bg-[#051c29] bg-opacity-80 backdrop-blur-sm text-white"
+                  : "bg-white bg-opacity-80 backdrop-blur-sm text-[#051c29]"
               }`}
             >
               <DropdownMenuLabel>
                 <div className="flex justify-between items-center">
-                  <p>Dark Mode</p>
+                  <p>{t("settings.DarkMode")}</p>
                   <Switch
                     id="airplane-mode"
                     checked={darkMode}
@@ -185,7 +185,9 @@ function MainLayout() {
                 </div>
               </DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuLabel>Select Language</DropdownMenuLabel>
+              <DropdownMenuLabel>
+                {t("settings.SelectLanguage")}
+              </DropdownMenuLabel>
               <DropdownMenuRadioGroup
                 value={selectedLanguage}
                 onValueChange={changeLanguage}
@@ -197,20 +199,20 @@ function MainLayout() {
                   <span className="mr-2">🇺🇸</span> EN
                 </DropdownMenuRadioItem>
                 <DropdownMenuRadioItem value="ko">
-                  <span className="mr-2">🇰🇷</span> KR
+                  <span className="mr-2">🇰🇷</span> KO
                 </DropdownMenuRadioItem>
               </DropdownMenuRadioGroup>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
 
-        <div className="flex flex-col items-center h-[calc(100vh-7rem)]">
+        <div className="flex flex-col items-center">
           <Outlet />
         </div>
       </div>
 
       {/* Sidebar */}
-      <aside className="sticky bottom-0 lg:top-6 lg:self-start lg:min-h-[calc(100vh-48px)] lg:w-28 w-full lg:rounded-xl border border-gray-200 shadow-sm xs:p-4 p-6 backdrop-blur-sm z-10 bg-opacity-75 bg-white dark:bg-opacity-75 dark:bg-gray-800 dark:border-gray-700">
+      <aside className="sticky bottom-0 lg:top-6 lg:self-start lg:min-h-[calc(100vh-48px)] lg:w-28 w-full lg:rounded-xl border border-gray-200 shadow-sm xs:p-4 p-6 backdrop-blur-sm z-10 bg-opacity-80 xs:bg-opacity-100 bg-white dark:bg-opacity-75 xs:dark:bg-opacity-100 dark:bg-gray-800 dark:border-gray-700">
         <div className="hidden lg:block text-center mb-8">
           <img
             src={ProfileLogo}
