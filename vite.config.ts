ec9d8abbd38de280f5faa84bfe706d83ab110d16
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2024-12-13 07:56:46 +0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import createCompressionPlugin from "vite-plugin-compression";
+import compression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    createCompressionPlugin({
-      verbose: true,
-      disable: false,
-      threshold: 10240,
+    compression({
       algorithm: "gzip",
       ext: ".gz",
     }),
