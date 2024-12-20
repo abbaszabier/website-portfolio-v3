ca3119e85b7d6939a3820fd444b5030f3fb9a1f2
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2024-12-20 07:40:35 +0700

    fix data

---
+++ src/lib/data.tsx
@@ -0,0 +1,108 @@
+import diyo from "@/assets/diyo.webp";
+
+const projectData = [
+  {
+    id: 1,
+    title: "DIYO POS",
+    type: "Real Project",
+    description:
+      "A point of sale application for restaurants and cafes built with React JS and Chakra UI",
+    imageUrl: diyo,
+    category: "react-js",
+    techStack: ["React JS", "React Query", "Tailwind CSS"],
+    linkRepo: "https://gitlab.com/",
+    linkWeb: "https://pos.diyo.app/",
+    screenshots: [
+      "https://via.placeholder.com/300x200",
+      "https://via.placeholder.com/300x200",
+      "https://via.placeholder.com/300x200",
+    ],
+  },
+  {
+    id: 2,
+    title: "Next JS Blog",
+    type: "Personal Project",
+    description:
+      "A personal blog platform built with Next JS and Tailwind CSS.",
+    imageUrl: "https://via.placeholder.com/300x200",
+    category: "next-js",
+    techStack: ["Next JS", "React Query", "Tailwind CSS"],
+    linkRepo: "https://x.com/abzabier_",
+    linkWeb: "https://x.com/abzabier_",
+    screenshots: [
+      "https://via.placeholder.com/300x200",
+      "https://via.placeholder.com/300x200",
+      "https://via.placeholder.com/300x200",
+    ],
+  },
+  {
+    id: 3,
+    title: "E-commerce Platform",
+    type: "Real Project",
+    description:
+      "An e-commerce website developed using React.js and Zustand for state management.",
+    imageUrl: "https://via.placeholder.com/300x200",
+    category: "react-js",
+    techStack: ["React JS", "React Query", "Tailwind CSS"],
+    linkRepo: "https://x.com/abzabier_",
+    linkWeb: "https://x.com/abzabier_",
+    screenshots: [
+      "https://via.placeholder.com/300x200",
+      "https://via.placeholder.com/300x200",
+      "https://via.placeholder.com/300x200",
+    ],
+  },
+  {
+    id: 4,
+    title: "Portfolio Website",
+    type: "Personal Project",
+    description:
+      "A responsive portfolio website created with React.js and Chakra UI.",
+    imageUrl: "https://via.placeholder.com/300x200",
+    category: "react-js",
+    techStack: ["Next JS", "React Query", "Tailwind CSS"],
+    linkRepo: "https://x.com/abzabier_",
+    linkWeb: "https://x.com/abzabier_",
+    screenshots: [
+      "https://via.placeholder.com/300x200",
+      "https://via.placeholder.com/300x200",
+      "https://via.placeholder.com/300x200",
+    ],
+  },
+  {
+    id: 5,
+    title: "Social Media App",
+    type: "Real Project",
+    description:
+      "A social media application built with React.js and Firebase for backend services.",
+    imageUrl: "https://via.placeholder.com/300x200",
+    category: "react-js",
+    techStack: ["Next JS", "React Query", "Tailwind CSS"],
+    linkRepo: "https://x.com/abzabier_",
+    linkWeb: "https://x.com/abzabier_",
+    screenshots: [
+      "https://via.placeholder.com/300x200",
+      "https://via.placeholder.com/300x200",
+      "https://via.placeholder.com/300x200",
+    ],
+  },
+  {
+    id: 6,
+    title: "E-learning Platform",
+    type: "Personal Project",
+    description:
+      "An online learning platform developed using Next JS and React Query for data fetching.",
+    imageUrl: "https://via.placeholder.com/300x200",
+    category: "next-js",
+    techStack: ["Next JS", "React Query", "Tailwind CSS"],
+    linkRepo: "https://x.com/abzabier_",
+    linkWeb: "https://x.com/abzabier_",
+    screenshots: [
+      "https://via.placeholder.com/300x200",
+      "https://via.placeholder.com/300x200",
+      "https://via.placeholder.com/300x200",
+    ],
+  },
+];
+
+export default projectData;
