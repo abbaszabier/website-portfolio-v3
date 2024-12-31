51ab82e8565b6c37485b4969b31559ef9a205b73
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2024-12-31 12:07:11 +0700

    implemen PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,40 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import compression from "vite-plugin-compression";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Portfolio Abbas",
+        short_name: "Portfolio Abbas",
+        description:
+          "Portfolio untuk menampilkan project yang telah saya kerjakan",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/profile192x192.webp",
+            sizes: "192x192",
+            type: "image/webp",
+          },
+          {
+            src: "/profile512x512.webp",
+            sizes: "512x512",
+            type: "image/webp",
+          },
+        ],
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      },
+    }),
     compression({
       algorithm: "gzip",
       ext: ".gz",
@@ -14,6 +43,7 @@ export default defineConfig({
   ],
   build: {
     minify: "terser",
+    chunkSizeWarningLimit: 3000,
   },
   resolve: {
     alias: {
