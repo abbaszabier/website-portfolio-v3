0dfcbb01ccea7b465a541595f9ad177a20cb23e2
Abbas Zabier Mohammad <abbas.zabier06@gmail.com>
2025-01-22 10:26:46 +0700

    update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b54saov03a8"
+    "revision": "0.lc6s9uhonlo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
